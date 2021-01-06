d3c7e499ffe905ce6b8ecc9c8ee6eaa66f31c80f
Nariman khaled <nariman.khaled@andela.com>
2021-01-06 05:21:02 +0200

    add Fibonacci Class + handling results

--- SWENSONChallenge/Helpers/FibonacciGenerator.swift
+++ SWENSONChallenge/Helpers/FibonacciGenerator.swift
@@ -8,5 +8,40 @@
 import Foundation
 
 class FibonacciGenerator {
+    
+    static func GenerateFibonacciValueFor(number : Int , UseIterative : Bool = true ) -> Int {
+        if number < 91 {
+            return UseIterative ? iterativeFibonacci(number) : recursiveFibonacci(n : number)
+        }
+        else {
+            return 0
+        }
+    }
 
+    static func iterativeFibonacci(_ n: Int) -> Int {
+        var fibs: [Int] = [0, 1]
+        (2...n).forEach { i in
+            fibs.append(fibs[i - 1] + fibs[i - 2])
+        }
+        return fibs.last!
+    }
+    
+    static func recursiveFibonacci( n :  Int,  val :  Int = 1,  prev :  Int = 0) -> Int
+    {
+        if(n == 0) {
+            return prev
+        }
+        return recursiveFibonacci(n:n - 1,val: val+prev,prev: val)
+    }
+    
+    
+    static func benchmark(method: () -> Void) {
+        let startTime = DispatchTime.now().uptimeNanoseconds
+        method()
+        let endTime = DispatchTime.now().uptimeNanoseconds
+
+        let elapsedTime = Double(endTime - startTime) / 1e6
+        print("Elapsed time: \(elapsedTime) milliseconds")
+    }
+    
 }
--- SWENSONChallenge/ViewControllers/Fibonacci generator/FibonacciGeneratorVC.swift
+++ SWENSONChallenge/ViewControllers/Fibonacci generator/FibonacciGeneratorVC.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class FibonacciGeneratorVC: UIViewController {
 
+    //MARK: IBOutlets
+    
+    @IBOutlet weak var txtUserInput: UITextField!
+    @IBOutlet weak var lblResult: UILabel!
+    
+    //MARK:- ViewController LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,6 +28,28 @@ class FibonacciGeneratorVC: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
-
+    @IBAction func GenerateValue(_ sender: Any) {
+        if let number  = txtUserInput.text {
+       
+        let num = Int(number) ?? 0
+        
+            let result = FibonacciGenerator.GenerateFibonacciValueFor(number :  num)
+            lblResult.text = "\(result)"
+            
+        FibonacciGenerator.benchmark {
+              // The first empty line is here is here simply for formatting.
+              print()
+            print("fib iterative: \(FibonacciGenerator.GenerateFibonacciValueFor(number :  num))")
+          }
+        
+        FibonacciGenerator.benchmark {
+              // The first empty line is here is here simply for formatting.
+              print()
+            print("fibRecursive: \(FibonacciGenerator.GenerateFibonacciValueFor(number:num , UseIterative : false))")
+          }
+        }
+        
+    }
+    
 
 }
